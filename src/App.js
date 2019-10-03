dee7b73b99d238b81b1e90117f8e70a021e37255
karatehcops <contact@lazers.tv>
2019-10-03 16:59:04 -0400

    Brain dump...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Attempting to create a reusable intersection observer...
+export const useIntersect = (ref, rootMargin = '0px') => {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState(false);
+  //
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        console.log(entry);
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      },
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return isIntersecting;
+};
 function App() {
+  const rootRect = useRef();
+  const thingToWatch = useRef();
+  // This reusable hook is not firing correctly when state
+  // updating quickly
+  // const entry = useIntersect(thingToWatch, '-50%');
+  //console.log(entry);
+
+  // This observer fires correctly when scrolling quickly
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      // Update our state when observer callback fires
+      console.log(entry);
+    },
+    {
+      rootMargin: '0px',
+    },
+  );
+  useEffect(() => {
+    observer.observe(thingToWatch.current);
+  });
   return (
-    <div className="App">
+    <div ref={rootRect} className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +64,12 @@ function App() {
         >
           Learn React
         </a>
+        <div style={{ height: '100vh' }} />
+        <div
+          ref={thingToWatch}
+          style={{ height: '10px', width: '10px', background: 'red' }}
+        />
+        <div style={{ height: '100vh' }} />
       </header>
     </div>
   );
