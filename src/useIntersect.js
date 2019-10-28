3b2c6bc383f56be6a3500b890659891ebcfac369
karatehcops <contact@lazers.tv>
2019-10-28 17:00:04 -0400

    Update intersection observer.

--- src/useIntersect.js
+++ src/useIntersect.js
@@ -1,51 +1,57 @@
 import { useState, useEffect, useRef } from 'react';
 
-export const useIntersect = ({
+export const useIntersection = ({
   root = null, // defaults to viewport when null.
   rootMargin, // expands or contracts the intersection root hit area
   threshold = 0, // intersection ratio value, also accepts arrays
   repeats = true,
   debug = false,
 } = {}) => {
+  const isWindowObservable =
+    typeof window !== 'undefined' && 'IntersectionObserver' in window;
   const [observerEntry, updateEntry] = useState({});
   const [node, setNode] = useState(null);
 
   const observer = useRef(
-    new window.IntersectionObserver(
-      ([currEntry]) => {
-        if (debug) {
-          console.log(currEntry); // eslint-disable-line no-console
-        }
-
-        // Only trigger once if repeat option is false
-        if (!repeats && currEntry.isIntersecting) {
-          // Disable the observer after the event fires
-          observer.current.disconnect();
-          updateEntry(currEntry);
-        }
-        if (repeats) {
-          updateEntry(currEntry);
-        }
-      },
-      {
-        root,
-        rootMargin,
-        threshold,
-      },
-    ),
+    isWindowObservable &&
+      new window.IntersectionObserver(
+        ([currEntry]) => {
+          if (debug) console.log(currEntry); // eslint-disable-line no-console
+          // Only trigger once if repeat option is false
+          if (!repeats && currEntry.isIntersecting) {
+            // Pull the observer after the event fires
+            observer.current.disconnect();
+            updateEntry(currEntry);
+          }
+          if (repeats) {
+            updateEntry(currEntry);
+          }
+        },
+        {
+          root,
+          rootMargin,
+          threshold,
+        },
+      ),
   );
 
   useEffect(() => {
-    const { current: currentObserver } = observer;
-    currentObserver.disconnect();
+    if (observer) {
+      const { current: currentObserver } = observer;
+      currentObserver.disconnect();
 
-    if (node) currentObserver.observe(node);
+      if (node) currentObserver.observe(node);
 
-    return () => currentObserver.disconnect();
+      return () => currentObserver.disconnect();
+    }
+
+    return () => {};
   }, [node]);
 
-  // Pass back method to set dom node and observer output
-  return [setNode, observerEntry];
+  return [
+    setNode, // Sets dom node to watch, expects a ref
+    observerEntry, // Intersection observer entry output
+  ];
 };
 
-export default useIntersect;
+export default useIntersection;
